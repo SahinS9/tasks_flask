b549c7c20054afb4918229e3302c3db4dd7a54e9
ShahinNettech <shahin.nettech@gmail.com>
2024-01-10 13:49:16 +0400

    forms created, not used in html

--- flask_beginning/flaskblog.py
+++ flask_beginning/flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '4d25c03a2652dbba3ed90d04b40004bc'
 
 posts = [
     {
@@ -33,6 +36,64 @@ def about():
         'title':'About'
     }
     return render_template('about.html', context = context)
+
+
+
+
+
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    title = 'Register'
+
+
+    context = {
+        'form': form,
+        'title' : title
+    }
+    return render_template('register.html', context = context)
+
+
+
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    title = 'Login'
+
+
+    context = {
+        'form': form,
+        'title' : title
+    }
+    return render_template('login.html', context = context)
+
+@app.route('/logout')
+def logout():
+    return "HELE qoshmamushuq"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
