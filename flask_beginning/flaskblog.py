a6696e6e26f49770a7ea78339e958aaacb3d6512
ShahinNettech <shahin.nettech@gmail.com>
2024-01-11 18:11:14 +0400

    Database created

--- flask_beginning/flaskblog.py
+++ flask_beginning/flaskblog.py
@@ -1,10 +1,49 @@
 from flask import Flask, render_template, flash, redirect, url_for, request
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
 from forms import RegistrationForm, LoginForm
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '4d25c03a2652dbba3ed90d04b40004bc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),unique=True,nullable=False)
+    email = db.Column(db.String(120),unique=True,nullable=False)
+    image_file = db.Column(db.String(20),nullable=False,default='default.jpg')
+    password = db.Column(db.String(60),nullable=False)
+    posts = db.relationship('Post',backref = 'author', lazy=True) #this is not column, this is just relationship! - can use post.author - will show User data!
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}','{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(100),nullable=False)
+    date_posted = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
+
+
+
+
+
+    
+
+
 
 posts = [
     {
@@ -72,6 +111,14 @@ def register():
 def login():
     form = LoginForm()
     title = 'Login'
+
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect( url_for('home'))
+        else:
+            flash('Check email or password', 'danger')
+
 
     return render_template('login.html', form = form, title = title)
 
