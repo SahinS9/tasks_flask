ed7a7a74b576f42a9aa5c5dae0156b8207751dd6
SahinS9 <shahinsaatov@gmail.com>
2024-02-03 17:20:44 +0400

    Package video is done

--- flask_beginning/flaskblog/__init__.py
+++ flask_beginning/flaskblog/__init__.py
@@ -11,4 +11,8 @@ app.config['SECRET_KEY'] = '4d25c03a2652dbba3ed90d04b40004bc'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
 db = SQLAlchemy(app)
 
+#for creation of db in terminal run: app.app_context().push() after importing app
+#then db.create_all() will work
+
+
 from flaskblog import routes
