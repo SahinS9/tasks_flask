2883221123dc9a0ceed7c8c1c2ddd42ef89565d4
SahinS9 <shahinsaatov@gmail.com>
2024-02-08 00:47:03 +0400

    pagination for home page and user_posts page, modelObject.iter_pages to take page_num

--- flask_beginning/db_add_data.py
+++ flask_beginning/db_add_data.py
@@ -15,3 +15,30 @@ for i in range(1,35):
 db.session.add_all(posts)
 db.session.commit()
 
+
+
+
+#########  NOTES   #########  
+## PAGINATION
+
+#in terminal
+
+# >>> posts = Post.query.paginate(page = 6, per_page = 2)
+# >>> for page in posts.iter_pages():
+# ...     print(page)
+# ... 
+# 1
+# 2
+# None
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# None
+# 16
+# 17
+
+#None is for not showing all pages, some from begin., some from end. and mid.abs
--- flask_beginning/flaskblog/routes.py
+++ flask_beginning/flaskblog/routes.py
@@ -12,7 +12,10 @@ from PIL import Image #to resize images for save up memory
 @app.route('/')
 @app.route('/home')
 def home():
-    posts = Post.query.order_by(Post.date_posted.desc()).all()
+    # posts = Post.query.order_by(Post.date_posted.desc()).all() #need to change it for pagination
+    page = request.args.get('page',default = 1, type = int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page = 4)
+
     return render_template('home.html', posts = posts)
 
 @app.route('/about')
@@ -192,4 +195,13 @@ def delete_post(post_id):
     flash('Your post has been deleted!','success')
     return redirect(url_for('home'))
 
+@app.route("/user/<string:username>")
+def user_posts(username):
+    # posts = Post.query.order_by(Post.date_posted.desc()).all() #need to change it for pagination
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page',default = 1, type = int)
+    posts = Post.query.filter_by(author = user)\
+                .order_by(Post.date_posted.desc())\
+                .paginate(page = page, per_page = 4)
 
+    return render_template('user_posts.html', posts = posts, user=user)
